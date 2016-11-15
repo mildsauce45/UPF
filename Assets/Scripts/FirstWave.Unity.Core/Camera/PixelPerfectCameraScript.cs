599c9a6803567d189804b01ae0d67d27cbbebd7e
mildsauce45 <jmpinchot@gmail.com>
2016-11-15 09:59:44 -0800

    added a pixel perfect camera script to the core library

---
+++ Assets/Scripts/FirstWave.Unity.Core/Camera/PixelPerfectCameraScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace FirstWave.Unity.Core.Camera
+{
+	[ExecuteInEditMode]
+	[RequireComponent(typeof(UnityEngine.Camera))]
+	public class PixelPerfectCameraScript : MonoBehaviour
+	{
+		public int pixelsPerUnit = 16;
+
+		private int lastHeight;
+		private int lastPPU;
+
+		private UnityEngine.Camera attachedCamera;
+
+		public UnityEngine.Camera Camera
+		{
+			get
+			{
+				if (attachedCamera == null)
+					attachedCamera = GetComponent<UnityEngine.Camera>();
+
+				return attachedCamera;
+			}
+		}
+
+		void Update()
+		{
+			if (lastHeight == Screen.height && lastPPU == pixelsPerUnit)
+				return;
+
+			lastHeight = Screen.height;
+			lastPPU = pixelsPerUnit;
+
+			Camera.orthographicSize = Screen.height / (pixelsPerUnit * 2f);
+		}
+	}
+}
